96b34c38fb94f92c908993cefb69062a1565c31b
atukurikavya <144264066+atukurikavya@users.noreply.github.com>
2024-04-05 16:00:11 +0530

    Add files via upload

---
+++ ARKS'/zay-shop-demo/js/product.js
@@ -0,0 +1,118 @@
+
+
+export const productData = [
+
+        {
+            id: `a1`,
+            type: `Living`,
+            brand: `Adidas`,
+            name: `Living Room Design`,
+            gender: `Men`,
+            color: [`Cloud White`, `Core Black`,`Dash Grey`],
+            size: [4, 5, 5.5],
+            sale: `Running`,
+            price: 360.00,
+            rating: 2.8,
+            new: false,
+            url: [`https://media.designcafe.com/wp-content/uploads/2022/07/29185240/industrial-rustic-living-room-in-earthy-tones.jpg`,'industrial-rustic-living-room-in-earthy-tones.jpg'],
+            description: `From a morning gym session to a day of errands to an evening with friends, keep your feet comfortable and your style on point in these adidas shoes. Pair them with your favorite running shorts for the ultimate athletic-casual vibe. A mesh upper helps you stay cool, while superlight cushioning cradles every step you take.
+
+            Made with a series of recycled materials, this upper features at least 50% recycled content. This product represents just one of our solutions to help end plastic waste.`,
+            specification: [`Regular fit`,`Sandwich mesh upper`,`Supportive no-sew overlays`,`Rubber outsole`,`Upper contains a minimum of 50% recycled content`,`LIGHTMOTION cushioning`,`Lace closure`]
+        },
+
+        {
+            id: `a1`,
+            type: `Living`,
+            brand: `Adidas`,
+            name: `Living Room Design`,
+            gender: `Men`,
+            color: [`Cloud White`, `Core Black`,`Dash Grey`],
+            size: [4, 5, 5.5],
+            sale: `Running`,
+            price: 250.00,
+            rating: 2.8,
+            new: false,
+            url: [`https://images.livspace-cdn.com/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/ond-1634120396-Obfdc/jfm-2024-1704560007-SGQ6F/living-room-1704796237-OxcYs/lr5-1-1707212954-G2eRh.jpg`,'lr5-1-1707212954-G2eRh.avif'],
+            description: `From a morning gym session to a day of errands to an evening with friends, keep your feet comfortable and your style on point in these adidas shoes. Pair them with your favorite running shorts for the ultimate athletic-casual vibe. A mesh upper helps you stay cool, while superlight cushioning cradles every step you take.
+
+            Made with a series of recycled materials, this upper features at least 50% recycled content. This product represents just one of our solutions to help end plastic waste.`,
+            specification: [`Regular fit`,`Sandwich mesh upper`,`Supportive no-sew overlays`,`Rubber outsole`,`Upper contains a minimum of 50% recycled content`,`LIGHTMOTION cushioning`,`Lace closure`]
+        },
+
+        {
+            id: `a1`,
+            type: `Living`,
+            brand: `Adidas`,
+            name: `Living Room Design`,
+            gender: `Men`,
+            color: [`Cloud White`, `Core Black`,`Dash Grey`],
+            size: [4, 5, 5.5],
+            sale: `Running`,
+            price: 100.00,
+            rating: 2.8,
+            new: false,
+            url: [`https://dlifeinteriors.com/wp-content/uploads/2022/09/living-room-design-apartment-interior-project-kochi-1024x576.jpg`,'industrial-rustic-living-room-in-earthy-tones.jpg'],
+            description: `From a morning gym session to a day of errands to an evening with friends, keep your feet comfortable and your style on point in these adidas shoes. Pair them with your favorite running shorts for the ultimate athletic-casual vibe. A mesh upper helps you stay cool, while superlight cushioning cradles every step you take.
+
+            Made with a series of recycled materials, this upper features at least 50% recycled content. This product represents just one of our solutions to help end plastic waste.`,
+            specification: [`Regular fit`,`Sandwich mesh upper`,`Supportive no-sew overlays`,`Rubber outsole`,`Upper contains a minimum of 50% recycled content`,`LIGHTMOTION cushioning`,`Lace closure`]
+        },
+
+
+        {
+            id: `a2`,
+            type: `Cement`,
+            brand: `Ultra Tech`,
+            name: `Cement`,
+            gender: `women`,
+            color: [`Cloud White`, `Core Black`,`Dash Grey`],
+            size: [5.5, 6, 7],
+            sale: `Running`,
+            price: 11.000,
+            rating: 4.4,
+            new: false,
+            url: [`https://5.imimg.com/data5/WE/KF/MY-16423774/ultratech-cement-ppc.jpg`,'download.jpeg'],
+            description: `Go ahead and check all the boxes with these adidas NMD_V3 Shoes. Breathable. Comfortable. Made with recycled materials. That's a yes to all three. Plus there's the style, like the perforated details, which is also where that breathability comes in. Playing with transparency, the 3-Stripes layer over the perforations, giving a glimpse at them underneath the monochrome signoff.
+
+            This shoe's upper is made with a high-performance yarn which contains at least 50% Parley Ocean Plastic — reimagined plastic waste, intercepted on remote islands, beaches, coastal communities and shorelines, preventing it from polluting our ocean. The other 50% of the yarn is recycled polyester.`,
+            specification: [`Lace closure`,`Textile upper`,`Textile lining`,`BOOST midsole`,`Rubber outsole`,`Yarn in upper contains at least 50% Parley Ocean Plastic and 50% recycled polyester`,`Imported`],
+        },
+
+        {
+            id: `a2`,
+            type: `Cement`,
+            brand: `Ultra Tech`,
+            name: `Cement`,
+            gender: `women`,
+            color: [`Cloud White`, `Core Black`,`Dash Grey`],
+            size: [5.5, 6, 7],
+            sale: `Running`,
+            price: 11.000,
+            rating: 4.4,
+            new: false,
+            url: [`https://assets.bldnxt.in/catalog/product/cache/1/image/a77c1558d860704591e3027d1ebed402/b/i/birla002.png`,'birla002.png'],
+            description: `Go ahead and check all the boxes with these adidas NMD_V3 Shoes. Breathable. Comfortable. Made with recycled materials. That's a yes to all three. Plus there's the style, like the perforated details, which is also where that breathability comes in. Playing with transparency, the 3-Stripes layer over the perforations, giving a glimpse at them underneath the monochrome signoff.
+
+            This shoe's upper is made with a high-performance yarn which contains at least 50% Parley Ocean Plastic — reimagined plastic waste, intercepted on remote islands, beaches, coastal communities and shorelines, preventing it from polluting our ocean. The other 50% of the yarn is recycled polyester.`,
+            specification: [`Lace closure`,`Textile upper`,`Textile lining`,`BOOST midsole`,`Rubber outsole`,`Yarn in upper contains at least 50% Parley Ocean Plastic and 50% recycled polyester`,`Imported`],
+        },
+        {
+            id: `a2`,
+            type: `Cement`,
+            brand: `Ultra Tech`,
+            name: `Cement`,
+            gender: `women`,
+            color: [`Cloud White`, `Core Black`,`Dash Grey`],
+            size: [5.5, 6, 7],
+            sale: `Running`,
+            price: 11.000,
+            rating: 4.4,
+            new: false,
+            url: [`https://static.wixstatic.com/media/e68837_a4f55c81f98843abbdc7f7c703a2ff8f~mv2.webp`,'download.jpeg'],
+            description: `Go ahead and check all the boxes with these adidas NMD_V3 Shoes. Breathable. Comfortable. Made with recycled materials. That's a yes to all three. Plus there's the style, like the perforated details, which is also where that breathability comes in. Playing with transparency, the 3-Stripes layer over the perforations, giving a glimpse at them underneath the monochrome signoff.
+
+            This shoe's upper is made with a high-performance yarn which contains at least 50% Parley Ocean Plastic — reimagined plastic waste, intercepted on remote islands, beaches, coastal communities and shorelines, preventing it from polluting our ocean. The other 50% of the yarn is recycled polyester.`,
+            specification: [`Lace closure`,`Textile upper`,`Textile lining`,`BOOST midsole`,`Rubber outsole`,`Yarn in upper contains at least 50% Parley Ocean Plastic and 50% recycled polyester`,`Imported`],
+        },
+        ]
